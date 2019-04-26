fbd5eab7ef756947c0fb1fb10de1f08d79db8403
Kirito <44616665+xxScurvyScarabxx@users.noreply.github.com>
2019-04-26 18:46:57 +0300

    Update Main.php

--- src/ShellyEssentials/Main.php
+++ src/ShellyEssentials/Main.php
@@ -32,7 +32,7 @@ use ShellyEssentials\tasks\ClearLaggTask;
 
 class Main extends PluginBase{
 
-	public const PREFIX = TextFormat::GRAY . TextFormat::BOLD . "(" . TextFormat::RED . "Chalix " . TextFormat::GRAY . ")" . TextFormat::RESET;
+	public const PREFIX = TextFormat::GRAY . TextFormat::BOLD . "(" . TextFormat::RED . "Chalix" . TextFormat::GRAY . ")" . TextFormat::RESET;
 
 	/** @var Main $instance */
 	protected static $instance;
